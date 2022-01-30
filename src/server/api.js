d6d1e40d170ad31b38e25ef9715905e50fc10b43
naveen kothuri <kothurinaveen.kn@gmail.com>
2022-01-30 14:12:25 +0530

    Adjusting the server

--- src/server/api.js
+++ src/server/api.js
@@ -2,13 +2,20 @@
 const compression = require('compression');
 const helmet = require('helmet');
 const express = require('express');
+const path = require('path');
+
+const DIST_DIR = './dist';
+const HOST = process.env.API_HOST || 'localhost';
+const PORT = process.env.API_PORT || 3001;
 
 const app = express();
+
+app.use(express.static(DIST_DIR));
 app.use(helmet());
 app.use(compression());
-
-const HOST = process.env.API_HOST || 'localhost';
-const PORT = process.env.API_PORT || 3002;
+app.use(/^(?!\/api).+/, (req, res) => {
+    res.sendFile(path.resolve(DIST_DIR, 'index.html'));
+});
 
 app.get('/api/v1/endpoint', (req, res) => {
     res.json({ success: true });
